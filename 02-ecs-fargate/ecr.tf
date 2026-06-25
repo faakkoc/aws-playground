9726b03a0dff530fa549406710999c545dd0c53f
Fatih Akkoc <akkoc.fatih@web.de>
2026-06-25 23:47:54 +0200

    fix: rename ecr repo & disable public

--- 02-ecs-fargate/ecr.tf
+++ 02-ecs-fargate/ecr.tf
@@ -1,8 +1,7 @@
 module "ecr" {
   source = "terraform-aws-modules/ecr/aws"
 
-  repository_name = "docker"
-  repository_type = "public"
+  repository_name = "fargate-task-docker-images"
 
   # repository_read_write_access_arns = ["arn:aws:iam::012345678901:role/terraform"]
   repository_image_tag_mutability = "MUTABLE"
